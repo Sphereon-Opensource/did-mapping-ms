df3a4eb6b016002643f826f36a27180948239e9e
Scott Malley <srmalley-sphereon@github.com>
2019-11-11 14:42:17 +0100

    fixed static import

--- src/main/java/com/sphereon/ms/did/mapping/maps/model/DidMapId.java
+++ src/main/java/com/sphereon/ms/did/mapping/maps/model/DidMapId.java
@@ -1,21 +1,22 @@
 package com.sphereon.ms.did.mapping.maps.model;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class DidMapId {
     private final String applicationId;
     private final String userId;
 
-    public DidMapId(DidMap didMap){
+    public DidMapId(DidMap didMap) {
         this.applicationId = didMap.getApplicationId();
         this.userId = didMap.getUserId();
     }
 
-    public static List<DidMapId> listFromDidMaps(List<DidMap> didMaps){
+    public static List<DidMapId> listFromDidMaps(List<DidMap> didMaps) {
         return didMaps.stream()
                 .map(DidMapId::new)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
     public String getApplicationId() {
